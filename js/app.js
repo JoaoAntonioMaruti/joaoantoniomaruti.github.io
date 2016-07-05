a70a3eb68e826ad23b95f8dbe2d90536f318e511
João Antonio Maruti Milagres <joaoantoniomaruti@gmail.com>
2016-07-05 19:29:22 +0000

    Parte do front-end

--- js/app.js
+++ js/app.js
@@ -4,14 +4,20 @@ var app = angular.module('app', [
     'ngRoute'
     , 'app.home'
     , 'app.comingSoon'
+    , 'app.parallax'
 ]);
 
 /**
 * 0 - Em breve
 * 1 - Teste
-* 3 - Produção
+* 2 - Produção
 */
-app.constant('ENVIRONMENT', 0);
+app.constant('ENVIRONMENT', 2);
+
+/**
+ * Cálculo da idade a partir do nascimento
+ */
+app.constant('AGE', Math.floor(Math.ceil(Math.abs(new Date('12/10/1994').getTime() - new Date().getTime()) / (1000 * 3600 * 24)) / 365.25));
 
 app.config(function($routeProvider){
      $routeProvider
--- js/controllers/home.js
+++ js/controllers/home.js
@@ -2,7 +2,12 @@
 /* global angular*/
 angular.module('app.home', [])
 
-.controller('home.ctrl', function($scope, $location, ENVIRONMENT){
+.controller('home.ctrl', function($scope, $location, ENVIRONMENT, Parallax, Nav, AGE){
+    Parallax.init();
+    Nav.init();
+    // Pegar  a idade
+    $scope.age = AGE;
+
     if(ENVIRONMENT == 0){
         $location.path('/comingSoon');
         return true;
