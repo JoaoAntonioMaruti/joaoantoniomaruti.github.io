77e6eb2d55197f5c1ce7bb9195ab3db2d7efad1e
Joao Antonio Maruti Milagres <joaoantoniomaruti@gmail.com>
2017-04-24 12:23:20 +0000

    Remove files

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-let express = require("express");
-let app = express();
-app.use(express.static(__dirname + '/public'));
-
-var port = Number( process.env.PORT || 8080 );
-
-app.listen(port);
