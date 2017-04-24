a452a76e9254673e8d87e7f4c30589403744bcb0
Joao Antonio Maruti Milagres <joaoantoniomaruti@gmail.com>
2017-04-24 12:26:56 +0000

    remove public directory

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const gulp = require('gulp'),
 gulp.task('sass', function () {
   return gulp.src('./SASS/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./css'));
 });
  
 gulp.task('sass:watch', function () {
