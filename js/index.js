e8e9d2fefa69c7d2365f84c4e3e4dbe52c7a0dce
Joao Milagres <joao.milagres@ntxdev.com.br>
2017-09-19 10:28:27 -0300

    redesign

--- js/index.js
+++ js/index.js
@@ -1,24 +1,16 @@
 'use strict';
 
-
-// $(document).ready(function(){
-//     $(function () {
-//         $('#back-top').click(function () {
-//             $('body,html').animate({
-//                 scrollTop: 0
-//             }, 800);
-//             return false;
-//         });
-//     });
-// });
-
 document.addEventListener('scroll', function(event) {
     var x = event.target.body.scrollTop,
         transY = (x * 0.5),
         scale = 1 + (x * 0.0012),
         transform = 'translateY('+transY+'px) scale('+scale+') translate3d(0,0,0)',
-        header = document.querySelector('.header-bg').style;
+        header = document.querySelector('.header-bg').style,
+        logo = document.querySelector('.profile__logo').style,
+        name = document.querySelector('.profile__name').style;
 
+    logo.opacity = 1 - (x * 0.0022);
+    name.opacity = 1 - (x * 0.0018);
     header.opacity = 1 - (x * 0.0008);
     header.WebkitTransform = transform;
     header.MozTransform = transform;
