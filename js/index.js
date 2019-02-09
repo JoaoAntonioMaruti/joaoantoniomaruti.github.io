81cd83e5be1c626b58cbcbe3a88457ebeca501b6
João Antonio <joaoantoniomaruti@gmail.com>
2019-02-09 00:47:25 -0200

    new header

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
-'use strict';
+document.addEventListener('scroll', function() {
+	const top = window.pageYOffset;
+	const one = document.querySelector('.one');
+	const two = document.querySelector('.two');
+	const three = document.querySelector('.three');
+	const four = document.querySelector('.four');
+	const five = document.querySelector('.five');
 
-document.addEventListener('scroll', function(event) {
-    var x = event.target.body.scrollTop,
-        transY = (x * 0.5),
-        scale = 1 + (x * 0.0012),
-        transform = 'translateY('+transY+'px) scale('+scale+') translate3d(0,0,0)',
-        header = document.querySelector('.header-bg').style,
-        logo = document.querySelector('.profile__logo').style,
-        name = document.querySelector('.profile__name').style;
-
-    logo.opacity = 1 - (x * 0.0022);
-    name.opacity = 1 - (x * 0.0018);
-    header.opacity = 1 - (x * 0.0008);
-    header.WebkitTransform = transform;
-    header.MozTransform = transform;
-    header.msTransform = transform;
-    header.transform= transform;
+	one.style.bottom = (-(top * 0.15) + 'px');
+	two.style.bottom = (-(top * 0.2) + 'px');
+	three.style.bottom = (-(top * 0.3) + 'px');
+	four.style.bottom = (-(top * 0.4) + 'px');
+	five.style.bottom = (-(top * 0.5) + 'px');
 });
