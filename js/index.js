aa5f8d54ffabd71d2a3fb5233b6233d3bd8f3140
João Antonio <joaoantoniomaruti@gmail.com>
2019-02-09 01:51:43 -0200

    remove jQuery dependency o/

--- js/index.js
+++ js/index.js
@@ -12,3 +12,21 @@ document.addEventListener('scroll', function() {
 	four.style.bottom = (-(top * 0.4) + 'px');
 	five.style.bottom = (-(top * 0.5) + 'px');
 });
+
+
+var button = document.getElementById('send');
+
+button.addEventListener('click', function () {
+  var name = document.getElementById('send').value
+  var email = document.getElementById('email').value
+  var subject = document.getElementById('subject').value
+  var message = document.getElementById('message').value
+
+  console.log(name)
+  console.log(email)
+  console.log(subject)
+  console.log(message)
+
+
+})
+
